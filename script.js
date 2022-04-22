b364f58c804969e79d0f63f5d2a9c5a6119d3245
Mohireza <mohireza50@gmail.com>
2022-04-22 12:29:10 +0430

    fixes

--- script.js
+++ script.js
@@ -2,18 +2,20 @@
 let score = 20;
 const random = Math.floor(Math.random() * 99) + 1;
 document.querySelector('.guessButton').addEventListener('click', () => {
-  if (Number(document.querySelector('.guess').value) > random) {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('div.square p').textContent = 'Enter a number!';
+  } else if (guess > random) {
     document.querySelector('div.square p').textContent = 'TOO HIGH';
     score--;
     document.querySelector('.Score').textContent = `Score: ${score}`;
-  } else if (Number(document.querySelector('.guess').value) < random) {
+  } else if (guess < random) {
     document.querySelector('div.square p').textContent = 'TOO LOW';
     score--;
     document.querySelector('.Score').textContent = `Score: ${score}`;
-  } else if (Number(document.querySelector('.guess').value) === random) {
+  } else if (guess === random) {
     document.querySelector('div.square p').textContent = 'BINGO!';
     document.querySelector('.Score').textContent = `Score: ${score}`;
   }
 });
 console.log(score);
-document.querySelector('.Score').textContent = `Score: ${score}`;
