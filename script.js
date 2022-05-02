91b1668116db304c8024de7f9a140eacd7869cbd
Mohireza <mohireza50@gmail.com>
2022-05-02 18:30:57 +0430

    V1.0

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 'use strict';
 let score = 20;
-const random = Math.floor(Math.random() * 99) + 1;
+let highScore = 0;
+const scoreReducer = () => {
+  if (score > 1) {
+    score--;
+  } else {
+    score = 0;
+    document.querySelector('div.square p').textContent = 'YOU LOST';
+    document.querySelector('.guessButton').style.display = 'none';
+    document.querySelector('.again').style.display = 'block';
+  }
+};
+let random = Math.floor(Math.random() * 99) + 1;
 document.querySelector('.guessButton').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     document.querySelector('div.square p').textContent = 'Enter a number!';
   } else if (guess > random) {
     document.querySelector('div.square p').textContent = 'TOO HIGH';
-    score--;
+    scoreReducer();
     document.querySelector('.Score').textContent = `Score: ${score}`;
   } else if (guess < random) {
     document.querySelector('div.square p').textContent = 'TOO LOW';
-    score--;
+    scoreReducer();
     document.querySelector('.Score').textContent = `Score: ${score}`;
-  } else if (guess === random) {
+  } else if (guess === random && score > 0) {
     document.querySelector('div.square p').textContent = 'BINGO!';
-    document.querySelector('.Score').textContent = `Score: ${score}`;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(
+        '.highScore'
+      ).textContent = `High Score: ${highScore}`;
+    }
+    document.querySelector('body').style.backgroundColor = 'rgb(36, 159, 56)';
+    document.querySelector('.guessButton').style.display = 'none';
+    document.querySelector('.again').style.display = 'block';
   }
 });
-console.log(score);
+document.querySelector('.again').addEventListener('click', () => {
+  random = Math.floor(Math.random() * 99) + 1;
+  score = 20;
+  document.querySelector('.Score').textContent = `Score: ${score}`;
+  document.querySelector('div.square p').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.guessButton').style.display = 'block';
+  document.querySelector('.again').style.display = 'none';
+  if (
+    document.querySelector('body').style.backgroundColor === 'rgb(36, 159, 56)'
+  ) {
+    document.querySelector('body').style.backgroundColor = 'var(--bg-clr)';
+  }
+});
